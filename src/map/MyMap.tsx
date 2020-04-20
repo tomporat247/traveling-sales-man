6a43bdf4e9c7fd5bc2a11bc8c8226501de6423c5
DESKTOP-73GVL8A\Tom <tomporat247@gmail.com>
2020-04-20 12:51:20 +0300

    show start tooltip

--- src/map/MyMap.tsx
+++ src/map/MyMap.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {LatLng} from "../types/lat-lng";
-import {Map, TileLayer, Marker, Polyline} from 'react-leaflet';
+import {Map, TileLayer, Marker, Polyline, Tooltip} from 'react-leaflet';
 
 const MyMap = (props: {
     points: LatLng[],
@@ -15,10 +15,13 @@ const MyMap = (props: {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {props.points.map(point => <Marker key={point[0].toString() + point[1].toString()} position={point}
-                                           onClick={(e: any) => props.onPointRemoved([e.latlng.lat, e.latlng.lng])}/>)}
-        {props.route && <Polyline positions={props.route} />}
-        {props.bestRouteEver && <Polyline color='red' positions={props.bestRouteEver} />}
+        {props.points.map((point: LatLng, index: number) =>
+            <Marker key={point[0].toString() + point[1].toString()} position={point}
+                    onClick={(e: any) => props.onPointRemoved([e.latlng.lat, e.latlng.lng])}>
+                {index === 0 && <Tooltip direction='right' offset={[-8, -2]} opacity={1} permanent>Start</Tooltip>}
+            </Marker>)}
+        {props.route && <Polyline positions={props.route}/>}
+        {props.bestRouteEver && <Polyline color='red' positions={props.bestRouteEver}/>}
     </Map>;
 
 export default MyMap;
